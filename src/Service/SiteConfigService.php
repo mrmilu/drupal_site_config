5aff533513fdf27ee44762ec512ccdabcb880dd6
Carlos Mella <carlos.mella@mrmilu.com>
2023-12-11 12:24:07 +0100

    Fix indexation.

--- src/Service/SiteConfigService.php
+++ src/Service/SiteConfigService.php
@@ -7,107 +7,107 @@ use Drupal\site_config\SiteConfigPluginManager;
 
 class SiteConfigService {
 
-    /**
-     * @var \Drupal\site_config\SiteConfigPluginManager
-     */
-    protected SiteConfigPluginManager $siteConfigManager;
+  /**
+   * @var \Drupal\site_config\SiteConfigPluginManager
+   */
+  protected SiteConfigPluginManager $siteConfigManager;
 
-    /**
-     * @param \Drupal\site_config\SiteConfigPluginManager $siteConfigManager
-     */
-    public function __construct(SiteConfigPluginManager $siteConfigManager) {
-        $this->siteConfigManager = $siteConfigManager;
+  /**
+   * @param \Drupal\site_config\SiteConfigPluginManager $siteConfigManager
+   */
+  public function __construct(SiteConfigPluginManager $siteConfigManager) {
+    $this->siteConfigManager = $siteConfigManager;
+  }
+
+  /**
+   * Get all config values.
+   *
+   * @return array
+   */
+  public function getSiteConfig(): array {
+    $data = [];
+
+    foreach ($this->siteConfigManager->getDefinitions() as $pluginId => $pluginDefinition) {
+      try {
+        /** @var \Drupal\site_config\SiteConfigInterface $plugin */
+        $plugin = $this->siteConfigManager->createInstance($pluginId);
+        $data[$pluginId] = $plugin->getValues();
+      }
+      catch (PluginException $e) {
+        \Drupal::logger('site_config')->error($e->getMessage());
+      }
     }
 
-    /**
-     * Get all config values.
-     *
-     * @return array
-     */
-    public function getSiteConfig(): array {
-        $data = [];
+    return $data;
+  }
 
-        foreach ($this->siteConfigManager->getDefinitions() as $pluginId => $pluginDefinition) {
-            try {
-                /** @var \Drupal\site_config\SiteConfigInterface $plugin */
-                $plugin = $this->siteConfigManager->createInstance($pluginId);
-                $data[$pluginId] = $plugin->getValues();
-            }
-            catch (PluginException $e) {
-                \Drupal::logger('site_config')->error($e->getMessage());
-            }
-        }
-
-        return $data;
+  /**
+   * Get config value by ID.
+   *
+   * @param string|null $id
+   *
+   * @return array
+   */
+  public function getSiteConfigById(?string $id): array {
+    if (empty($id) || !$this->siteConfigManager->hasDefinition($id)) {
+      return [];
     }
 
-    /**
-     * Get config value by ID.
-     *
-     * @param string|null $id
-     *
-     * @return array
-     */
-    public function getSiteConfigById(?string $id): array {
-        if (empty($id) || !$this->siteConfigManager->hasDefinition($id)) {
-            return [];
-        }
-
-        try {
-            /** @var \Drupal\site_config\SiteConfigInterface $plugin */
-            $plugin = $this->siteConfigManager->createInstance($id);
-            return $plugin->getValues();
-        }
-        catch (PluginException $e) {
-            \Drupal::logger('site_config')->error($e->getMessage());
-            return [];
-        }
+    try {
+      /** @var \Drupal\site_config\SiteConfigInterface $plugin */
+      $plugin = $this->siteConfigManager->createInstance($id);
+      return $plugin->getValues();
     }
-
-    /**
-     * Get value.
-     *
-     * @param $siteKey
-     * @param $field
-     *
-     * @return array|mixed
-     */
-    public function getValue($siteKey, $field) {
-        if (empty($siteKey) || !$this->siteConfigManager->hasDefinition($siteKey)) {
-            return [];
-        }
-        try {
-            /** @var \Drupal\site_config\SiteConfigInterface $plugin */
-            $plugin = $this->siteConfigManager->createInstance($siteKey);
-            return $plugin->getValue($field);
-        }
-        catch (PluginException $e) {
-            \Drupal::logger('site_config')->error($e->getMessage());
-            return [];
-        }
+    catch (PluginException $e) {
+      \Drupal::logger('site_config')->error($e->getMessage());
+      return [];
     }
+  }
 
-    /**
-     * Set value.
-     *
-     * @param $siteKey
-     * @param $field
-     * @param $value
-     *
-     * @return void
-     */
-    public function setValue($siteKey, $field, $value) {
-        if (empty($siteKey) || !$this->siteConfigManager->hasDefinition($siteKey)) {
-            return;
-        }
-        try {
-            /** @var \Drupal\site_config\SiteConfigInterface $plugin */
-            $plugin = $this->siteConfigManager->createInstance($siteKey);
-            $plugin->setValue($field, $value);
-        }
-        catch (PluginException $e) {
-            \Drupal::logger('site_config')->error($e->getMessage());
-        }
+  /**
+   * Get value.
+   *
+   * @param $siteKey
+   * @param $field
+   *
+   * @return array|mixed
+   */
+  public function getValue($siteKey, $field) {
+    if (empty($siteKey) || !$this->siteConfigManager->hasDefinition($siteKey)) {
+      return [];
     }
+    try {
+      /** @var \Drupal\site_config\SiteConfigInterface $plugin */
+      $plugin = $this->siteConfigManager->createInstance($siteKey);
+      return $plugin->getValue($field);
+    }
+    catch (PluginException $e) {
+      \Drupal::logger('site_config')->error($e->getMessage());
+      return [];
+    }
+  }
+
+  /**
+   * Set value.
+   *
+   * @param $siteKey
+   * @param $field
+   * @param $value
+   *
+   * @return void
+   */
+  public function setValue($siteKey, $field, $value) {
+    if (empty($siteKey) || !$this->siteConfigManager->hasDefinition($siteKey)) {
+      return;
+    }
+    try {
+      /** @var \Drupal\site_config\SiteConfigInterface $plugin */
+      $plugin = $this->siteConfigManager->createInstance($siteKey);
+      $plugin->setValue($field, $value);
+    }
+    catch (PluginException $e) {
+      \Drupal::logger('site_config')->error($e->getMessage());
+    }
+  }
 
 }
